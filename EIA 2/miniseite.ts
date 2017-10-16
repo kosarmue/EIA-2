5a896cef60225ed353f19da624851122dd31129e
kosarmue <32566225+kosarmue@users.noreply.github.com>
2017-10-16 10:52:59 +0200

    Delete miniseite.ts

--- EIA 2/miniseite.ts
+++
@@ -1,7 +0,0 @@
-function dialogFeld() {
-    var eingabe = prompt("Hi ich bin...", "Name");
-    if (eingabe != null) {
-        document.getElementById("hi").innerHTML =
-           "Hi" + eingabe + "wie geht´s";
-    }
-}
