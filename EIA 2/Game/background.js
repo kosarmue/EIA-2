90a6d5a7df3be65dbcf848babd2a69e7b9b6e595
kosarmue <32566225+kosarmue@users.noreply.github.com>
2018-02-23 17:06:31 +0100

    Add files via upload

---
+++ EIA 2/Game/background.js
@@ -0,0 +1,12 @@
+"use strict";
+class background {
+    constructor() {
+        this.draw();
+    }
+    draw() {
+        crc2.fillStyle = "white";
+        crc2.fillRect(0, 0, 600, 424);
+    }
+}
+exports.background = background;
+//# sourceMappingURL=background.js.map
---
+++ EIA 2/Game/background.ts
@@ -0,0 +1,10 @@
+export class background {
+        constructor() {
+            this.draw();
+        }
+
+        draw(): void {
+            crc2.fillStyle = "white";
+            crc2.fillRect(0, 0, 600, 424);
+            }
+}
