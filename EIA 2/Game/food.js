ca16cee6a2ff4f7b722f3a804513a9244411b466
kosarmue <32566225+kosarmue@users.noreply.github.com>
2018-02-23 18:33:47 +0100

    Add files via upload

---
+++ EIA 2/Game/food.js
@@ -0,0 +1,12 @@
+"use strict";
+class food {
+    constructor() {
+        this.draw();
+    }
+    draw() {
+        crc2.fillStyle = 'blue';
+        crc2.fillRect(x * 10, y * 10 + 20, 10, 10);
+    }
+}
+exports.food = food;
+//# sourceMappingURL=food.js.map
