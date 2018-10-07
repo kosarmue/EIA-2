933cbbd966f80cfa1fb89943e4b44e0e65eccd08
kosarmue <32566225+kosarmue@users.noreply.github.com>
2018-10-07 14:49:20 +0200

    Update miniseite.ts

--- EIA 2/Aufgabe-0/miniseite.ts
+++ EIA 2/Aufgabe-0/miniseite.ts
@@ -2,6 +2,6 @@ function dialogFeld() {
     var eingabe = prompt("Hi ich bin...", "Name");
     if (eingabe != null) {
         document.getElementById("hi").innerHTML =
-           "Hi" + eingabe + "was geht";
+           "Hi " + eingabe + " was geht";
     }
 }
