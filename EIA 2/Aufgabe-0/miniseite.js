7ae03ac394d9d3d7ebe6af2123a1fdc6dbd55fe7
kosarmue <32566225+kosarmue@users.noreply.github.com>
2018-10-12 16:48:17 +0200

    Delete miniseite.js

--- EIA 2/Aufgabe-0/miniseite.js
+++
@@ -1,9 +0,0 @@
-function dialogFeld() {
-    var eingabe = prompt("Hi ich bin...", "Name");
-    if (eingabe != null) {
-        document.getElementById("hi").innerHTML =
-            "Hi, " + eingabe + " was geht?";
-    }
-    console.log("Hey ", eingabe, ", alles gut?");
-}
-//# sourceMappingURL=miniseite.js.map
